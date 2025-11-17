beef337bedbcd5db2443a853767eee67657eafc6
= <--replace-all>
2025-11-17 12:02:28 +0500

    yangi malumot qo'shildi

--- src/routes/adduser.tsx
+++
@@ -1,7 +0,0 @@
-import AddUser from '@/pages/AddUser'
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/adduser')({
-  component: AddUser,
-})
-
--- src/routes/userdetail.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import UserDetail from "@/pages/UserDetail"; // pages ichidagi fayl
-
-export const Route = createFileRoute("/userdetail")({
-  component: UserDetail,
-  validateSearch: (search: Record<string, unknown>) => ({
-    id: search.id ? Number(search.id) : undefined,
-  }),
-});
